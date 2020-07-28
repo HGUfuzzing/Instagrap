04129eb4fb5f80c0a3f8c3c7025e763d7a320904
LeeHyunji <leehyunji0715@gmail.com>
2020-07-28 15:59:37 +0900

    First branch commit

--- instagrapd.c
+++ instagrapd.c
@@ -1,1 +1,86 @@
 // Instagrap.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+
+#define BUFFUNIT 1024
+
+int main (int argc, char *argv[]) {
+    FILE *fp = NULL;
+    int i, bytes, tot_bytes, str_len;
+    int serv_sock, clnt_sock;
+    // int worker_port = argv[4]; - ip:port 분리해야함
+    int serv_port = atoi(argv[2]);
+    char userID[BUFFUNIT];
+    char recvbuff[BUFFUNIT];
+
+    if (argc != 6) {
+        printf("Usage: %s -p <port number> -w <IP>:<port> <testcase_path>");
+    }
+
+    struct sockaddr_in serv_addr, clnt_addr;
+    socklen_t clnt_addr_size;
+
+    serv_sock = socket (PF_INET, SOCK_STREAM, 0);
+    if(serv_sock == -1)
+        error_handling("socket() error!");
+
+    memset (&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl (INADDR_ANY);
+    serv_addr.sin_port = htons (serv_port);
+
+    //완료
+    if(bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
+        error_handling("bind() error!");
+    if(listen (serv_sock, 100) == -1)
+        error_handling("listen() error!");
+
+    for(i=0; i<100; i++) {
+        clnt_sock = -1;
+        memset (&clnt_addr, 0, sizeof(clnt_addr));
+        clnt_sock = accept (serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
+        // <-- 여기 위치 쯤 클라이언트가 이미 전달된 유저인지 판별해야 한다.
+        //checkuser();
+        printf("Clients' socket: %d\n", clnt_sock);
+
+        if(clnt_sock == -1){
+            perror("accept() error!");
+            // error_handling("accept() error!"); 
+            //--> 수정해야함..? 연결중 다른 유저가 request 시에 처리법
+        }
+        else{
+            //Submitter의 정보 받기
+            read(clnt_sock, recvbuff, sizeof(recvbuff));
+            strncpy(userID, recvbuff, BUFFUNIT);
+            printf("UserID: %s\n", userID);
+
+            //FILE 받기
+            tot_bytes = 0;
+            fp = fopen("submitter_file.c","wb");
+            if(fp == NULL)
+                error_handling("fopen() error! You should check your file name");
+            
+            while((str_len = read(clnt_sock, recvbuff, BUFFUNIT)) != 0){
+                printf("recbuff: \n\n%s\n", recvbuff);
+                fprintf(fp, recvbuff);
+            }
+            fclose(fp);
+        }
+        close(clnt_sock);
+    }
+    close(serv_sock);
+}
+
+void error_handling(char * message){
+    fputs(message, stderr);
+    fputc('\n',stderr);
+    exit(1);
+}
--- submitter.c
+++ submitter.c
@@ -1,1 +1,95 @@
 //submitter.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+
+#define BUFFUNIT 1024 // 100?
+
+int main (int argc, char *argv[]) {
+
+    int clnt_sock;
+    int n=0, bytes;
+    int serv_port;
+    char user_info [BUFFUNIT];
+    char * serv_ip = NULL;
+    char sendbuffer[BUFFUNIT];
+
+    struct sockaddr_in serv_addr;
+
+    if(argc != 6){
+        printf("%s -n <server ip>:<port> -u <userID> <filename>\n", argv[0]);
+    }
+    else if (!strcmp(argv[1],"-n")) {
+
+    }
+    strcpy(user_info, argv[4]);
+
+    serv_ip = strtok(argv[2],":");
+    serv_port = atoi(strtok(NULL, ":"));
+    printf("Server: %s\nPort: %d\n\n", serv_ip, serv_port);
+    
+    clnt_sock = socket(PF_INET, SOCK_STREAM, 0);
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(serv_ip);
+    serv_addr.sin_port = htons(serv_port); 
+
+    //Connect1: Sending User ID & a file 
+    if( connect(clnt_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+        error_handling("connect() error!");
+    else
+        puts("Connected..............");
+    
+
+    write (clnt_sock, user_info, sizeof(user_info)); /* Send user's ID */
+
+    FILE * fp = fopen(argv[5], "rb"); /* Send user's file */
+    if(fp == NULL)
+        error_handling("fopen() error!");
+
+    while( (bytes = fread(sendbuffer, 1, sizeof(sendbuffer), fp)) >0 ){
+        printf("Send %d bytes\n", bytes);
+        if( send(clnt_sock, sendbuffer, bytes, 0) == -1 )
+            error_handling("send() error!");
+    }
+
+
+    //OR send request message / 2sec - 그 후 서버가 체크
+    close(clnt_sock);
+
+    //Connect2?????: requesting for results / 3sec
+    printf("Another Connection request for results\n");
+    clnt_sock = socket(PF_INET, SOCK_STREAM, 0);
+    while(1){
+        if(connect(clnt_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+            printf("Connection failed.....................Wait for a time");
+        else{
+            printf("Connected! You'll get the result\n");
+            /* *
+             * Reques
+             * 여기다가 Server로 부터 결과 받는
+             *
+             * */
+        }
+        sleep(3);
+    }
+    
+
+    fclose(fp);
+
+    return 0;
+}
+
+void error_handling(char * message){
+    fputs(message, stderr);
+    fputc('\n',stderr);
+    exit(1);
+}
