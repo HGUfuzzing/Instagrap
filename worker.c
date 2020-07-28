991507a3e9e8c11388bbf0561eedfad2908586d1
HyeonseopJeong <21600665@handong.edu>
2020-07-28 23:15:52 +0900

    make worker into server.

--- worker.c
+++ worker.c
@@ -6,87 +6,106 @@
 #include <sys/socket.h>
 #define BUF_SIZE 1024
 
+//Server!!!
 void error_handling(char* message);
 int send_file(int socket, const char * filename);
-int recv_file(int socket, char * filename);
+int recv_file(int socket, const char * filename);
 
 
 int main(int argc, char * argv[]) {
+    
+    char buf[BUF_SIZE];
+    int serv_sock;
     int clnt_sock;
+
+    //sockaddr_in은 소켓 주소의 틀을 형성해주는 구조체로 AF_INET일 경우 사용
     struct sockaddr_in serv_addr;
-    char message[1024] = {0,};
+    struct sockaddr_in clnt_addr;   //accept함수에서 사용됨.
+    socklen_t clnt_addr_size = sizeof(clnt_addr);
 
-    int status;
-    char path[1000];
-
-    if(argc != 4) {
-        printf("Usage : %s <ip address> <port> <worker space>\n", argv[0]);
+    if(argc != 3) {
+        printf("Usage : %s <port> <worker space>\n", argv[0]);
         exit(1);
     }
 
-    //TCP연결지향형이고 ipv4 도메인을 위한 소켓을 생성
-    clnt_sock = socket(PF_INET, SOCK_STREAM, 0);
-    if(clnt_sock == -1) 
+    //TCPd연결지향형이고 ipv4 도메인을 위한 소켓을 생성
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+    if(serv_sock == -1)
         error_handling("socket error");
     
-    //인자로 받은 서버주소 정보를 저장
+    //주소를 초기화한 후 IP주소와 포트 지정
     memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;  //서버주소체계는 ipv4.
-    serv_addr.sin_addr.s_addr = inet_addr(argv[1]); //서버주소(ip)저장
-    serv_addr.sin_port = htons(atoi(argv[2]));  //서버 포트번호 저장
+    serv_addr.sin_family=AF_INET;                   //타입: ipv4
+    serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);    //ip주소
+    serv_addr.sin_port=htons(atoi(argv[1]));         //port
 
-    //클라이언트 소켓부분에 서버를 연결
-    if(connect(clnt_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
-        error_handling("connect error");
+    //소켓과 서버 주소를 바인딩
+    if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr))==-1)
+        error_handling("bind error");
 
-    char buf[BUF_SIZE];
+    //연결 대기열 50개 생성
+    if(listen(serv_sock, 50)==-1)
+        error_handling("listen error");
+    printf("listening..\n");    
 
-    snprintf(buf, sizeof(buf), "%s/new.c", argv[3]);
 
-    //연결이 성공적으로 되었으면 데이터 받기
-    recv_file(clnt_sock, buf);
+    while(1) {
+        //클라이언트로부터 요청이 오면 연결 수락
+        clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);   //블록킹!  
+        if(clnt_sock==-1)
+            error_handling("accept error");
+        printf("Connection Request from Client [IP:%s, Port:%d] has been Accepted\n", inet_ntoa(clnt_addr.sin_addr), ntohs(clnt_addr.sin_port));
 
-    
-    FILE * fp;
+        snprintf(buf, sizeof(buf), "%s/new.c", argv[2]);
 
-    //받은 (.c)파일 컴파일 후 실행하기
-    snprintf(buf, sizeof(buf), "gcc -w %s/new.c 2> /dev/stdout", argv[3]);
-    
-    if((fp = popen(buf, "r")) == NULL)
-        error_handling("popen() error (1)");
-    memset(buf, 0, sizeof(buf));
-    fgets(buf, sizeof(buf), fp);
-    
-    send(clnt_sock, buf, 1, 0);    //compile error 여부 알려주기
-
-    if(strlen(buf) != 0) {
-        buf[0] = 0x0;
-        error_handling("compile error");
-    }
-    pclose(fp);
-    
-    while(fgets(buf, sizeof(buf), fp))
-        printf("%s", buf);    
-
-    for(int i = 1; i <= 5; i++) {
-        //input file 받고
-        snprintf(buf, sizeof(buf), "%s/%d.in", argv[3], i);
+        //연결이 성공적으로 되었으면 file 받기
         recv_file(clnt_sock, buf);
+        
+        FILE * fp;
 
-        //돌려보고
-        snprintf(buf, sizeof(buf), "./a.out < %s/%d.in > %s/%d.out", argv[3], i, argv[3], i);
-        if((fp = popen(buf, "r")) == NULL) 
-            error_handling("popen() error (2)");
+        //받은 (.c)파일 컴파일 후 실행하기
+        snprintf(buf, sizeof(buf), "gcc -w %s/new.c 2> /dev/stdout", argv[2]);
+        
+        if((fp = popen(buf, "r")) == NULL)
+            error_handling("popen() error (1)");
+        memset(buf, 0, sizeof(buf));
+        fgets(buf, sizeof(buf), fp);
+        
+        send(clnt_sock, buf, 1, 0);    //compile error 여부 알려주기
 
-        //output file 전송.
-        snprintf(buf, sizeof(buf), "./%s/%d.out", argv[3], i);
-        send_file(clnt_sock, buf);
+        if(strlen(buf) != 0) 
+            error_handling("compile error");
+
         pclose(fp);
-    }
 
+        for(int i = 1; i <= 5; i++) {
+            //input file 받고
+            snprintf(buf, sizeof(buf), "%s/%d.in", argv[2], i);
+            recv_file(clnt_sock, buf);
+
+            //돌려보고
+            snprintf(buf, sizeof(buf), "./a.out < %s/%d.in > %s/%d.out", argv[2], i, argv[2], i);
+            if((fp = popen(buf, "r")) == NULL) 
+                error_handling("popen() error (2)");
+            //시간 기다리고 timeout시 처리.
+            sleep(1);
+
+            //output file 전송.
+            snprintf(buf, sizeof(buf), "./%s/%d.out", argv[2], i);
+            send_file(clnt_sock, buf);
+            pclose(fp);
+        }
+
+        //생성된 파일 삭제.(worker space의 파일들 삭제)
+        snprintf(buf, sizeof(buf), "rm -rf %s/*", argv[2]);
+        fp = popen(buf, "r");
+        pclose(fp);
+        close(clnt_sock);
+    }
 
     //소켓 닫기
-    close(clnt_sock);
+    
+    close(serv_sock);
     puts("FINISHED");
     return 0;
 }
@@ -136,7 +155,7 @@ int send_file(int socket, const char * filename) {
 }
 
 
-int recv_file(int socket, char * filename) {
+int recv_file(int socket, const char * filename) {
     /*
         receive할 파일의 size를 먼저 보냄.
     */
